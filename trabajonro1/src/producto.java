60b64a586b53a04dd83d8845ed19d38d5cef86df
Federico Martinez <fedemarr83@gmail.com>
2026-08-13 17:07:31 -0300

    ejercicio 1 a y casi B

--- trabajonro1/src/producto.java
+++ trabajonro1/src/producto.java
@@ -1,5 +1,10 @@
 public class producto {
     int codigo=10;
-    String nombre="fede";
+    String nombre="impresora";
     int precio=100;
+    int cantidad=5;
+
+    public producto(int codigo,String nombre,int precio,int cantidad) {
+
+    }
 }
